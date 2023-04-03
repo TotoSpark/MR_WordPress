5ddc050d68e4c5d5dd4a12e69654634176140550
TotoSpark <matheoroma@hotmail.fr>
2023-04-03 19:12:23 +0200

    Affichange des pays dans le select front

--- wp-content/plugins/PROJETMR/classes/shortcodes/PROJETMR_Shortcodes_Choix.php
+++ wp-content/plugins/PROJETMR/classes/shortcodes/PROJETMR_Shortcodes_Choix.php
@@ -6,23 +6,39 @@ class PROJETMR_Shortcodes_Choix {
 
     static function display($atts) {
 
+        global $wpdb;
+
+        $db = $wpdb->prefix . PROJETMR_BASENAME . '_pays';
+
+        $sql_data = "SELECT id, pays FROM `$db` WHERE actif = 1;";
+
+        $result = $wpdb->get_results($sql_data, 'ARRAY_A');
+
+        $pays = "";
+
+        foreach ($result as $valeur) {
+
+            $pays .= "<option value=" . $valeur['id'] . ">" . $valeur['pays'] . "</option>";
+
+        }
+
         return "
         <form id=\"choix\" method=\"POST\">
             <fieldset>
                     <select id=\"choix1\" required>
-                        <option value=\"Choix\">Choix 1</option>
+                        <option value=\"Choix\">Choix 1</option>". $pays ."
                     </select>
                     <select id=\"choix2\" disabled>
-                        <option value=\"Choix\">Choix 2</option>
+                        <option value=\"Choix\">Choix 2</option>". $pays ."
                     </select>
                     <select id=\"choix3\" disabled>
-                        <option value=\"Choix\">Choix 3</option>
+                        <option value=\"Choix\">Choix 3</option>". $pays ."
                     </select>
                     <select id=\"choix4\" disabled>
-                        <option value=\"Choix\">Choix 4</option>
+                        <option value=\"Choix\">Choix 4</option>". $pays ."
                     </select>
                     <select id=\"choix5\" disabled>
-                        <option value=\"Choix\">Choix 5</option>
+                        <option value=\"Choix\">Choix 5</option>". $pays ."
                     </select>
                 </fieldset>
             <button id=\"submit\" type=\"submit\">bouton</button>
